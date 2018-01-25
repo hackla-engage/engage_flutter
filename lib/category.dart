e869d0550a7ef43e47c26e954ab71a7bfc46ea75
hungrymonkey <psuedofinnish@psuedos-MacBook-Pro.local>
2018-01-24 20:44:01 -0800

    temporary put categoryentry in category.dart for now

--- lib/category.dart
+++ lib/category.dart
@@ -18,9 +18,17 @@ class MyCategory extends StatefulWidget {
   _MyCategoryPageState createState() => new _MyCategoryPageState();
 }
 
-
-
-
+const List<CategoryEntry> _choices = const<CategoryEntry>[
+  const CategoryEntry("Infrastructure", Icons.store, "Infastructure would include agendas dealing " +
+  "with basic structures and facilities (e.g., buildings, roads, and power) needed for the city"
+  ),
+  const CategoryEntry("Bicycle", Icons.directions_bike, "bikes bikes bikes bikes"
+  ),
+  const CategoryEntry("Housing", Icons.home, "mi cassa, tu cassa"
+  ),
+  const CategoryEntry("Environment", Icons.bug_report, "environment"
+  ),
+];
 
 
 
