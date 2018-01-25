9566c2bf0c4525b66480b5e8d665a1da2746c537
hungrymonkey <psuedofinnish@psuedos-MacBook-Pro.local>
2018-01-24 21:03:08 -0800

    started the async categorylist queries

--- lib/loaders/category_loader.dart
+++ lib/loaders/category_loader.dart
@@ -1,7 +1,33 @@
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../shared/category_entry.dart';
 
-const CATEGORY_URL = 'https://council-tag.herokuapp.com/api/tags/?format=json';
 
+
+
+class CategoryList{
+
+  List<CategoryEntry> entries;
+
+  CategoryList(Map map){
+    entries = _extractEntries(map);
+  }
+
+  static List<CategoryEntry> _extractEntries(Map map){
+    List<CategoryEntry> l;
+    map.forEach(
+
+    )
+
+    return l;
+  }
+  
+  static Future<CategoryList> fetch() async {
+    const CATEGORY_URL = 'https://council-tag.herokuapp.com/api/tags/?format=json';
+    print( "***** query ${CATEGORY_URL} ******" );
+    final Map map = JSON.decode( await http.read(CATEGORY_URL) );
+    return new CategoryList(map);
+  }
+}
--- lib/shared/category_entry.dart
+++ lib/shared/category_entry.dart
@@ -3,21 +3,10 @@ import 'package:flutter/material.dart';
 
 
 class CategoryEntry {
-  const CategoryEntry(this.title, this.icon, this.description);
+  const CategoryEntry(this.id, this.title, this.icon, this.description);
   final int id;
   final String title;
   final IconData icon;
   final String description;
 }
 
-const List<CategoryEntry> _choices = const<CategoryEntry>[
-  const CategoryEntry("Infrastructure", Icons.store, "Infastructure would include agendas dealing " +
-  "with basic structures and facilities (e.g., buildings, roads, and power) needed for the city"
-  ),
-  const CategoryEntry("Bicycle", Icons.directions_bike, "bikes bikes bikes bikes"
-  ),
-  const CategoryEntry("Housing", Icons.home, "mi cassa, tu cassa"
-  ),
-  const CategoryEntry("Environment", Icons.bug_report, "environment"
-  ),
-];
