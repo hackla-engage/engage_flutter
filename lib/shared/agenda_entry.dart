f944598c2389826476967e7a1b1f7e529df742de
hungrymonkey <psuedofinnish@psuedos-MBP.lan>
2018-01-31 22:37:54 -0800

    added AgendaEntry dart

---
+++ lib/shared/agenda_entry.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AgendaEntry {
+  final int id;
+  final String title;
+  final String summary;
+  final String department;
+  final String background;
+  final String supplemental;
+  final String sponsors;
+  final int agenda;
+  const AgendaEntry(this.id, this.title, this.summary, this.department, this.background, this.supplemental, this.sponsors, this.agenda);
+}
