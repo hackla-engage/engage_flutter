189b4136b24e7d97385c3cd5c796740607eb9009
hungrymonkey <psuedofinnish@psuedos-MacBook-Pro.local>
2018-05-07 19:22:30 -0700

    change email

--- test/test_login.dart
+++ test/test_login.dart
@@ -8,9 +8,9 @@ void main() {
 	  //User alwaysWork = await UserHelper.loginUser("test0@test.com", "password");
     //expect( "test0@test.com" , alwaysWork.email);
 
-    User onlyPassOnce = await UserHelper.loginUser("monkeyboyted@yahoo.com", "password");
+    User onlyPassOnce = await UserHelper.loginUser("test4@test.com", "password");
 
-    var answer = "monkeyboyted@yahoo.com";
+    var answer = "test4@test.com";
     expect(answer, onlyPassOnce.email);
 
   });
