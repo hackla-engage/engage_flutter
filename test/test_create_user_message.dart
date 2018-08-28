a0a945967198031aaeae57ad266fd4960a57810f
hungrymonkey <psuedofinnish@psuedos-MacBook-Pro.local>
2018-08-27 20:24:27 -0700

    added more code

---
+++ test/test_create_user_message.dart
@@ -0,0 +1,18 @@
+import 'package:test/test.dart';
+
+import 'package:engage_flutter/shared/user.dart';
+import 'package:engage_flutter/posts/user_helper.dart';
+import 'package:engage_flutter/posts/messenger_helper.dart';
+
+void main() {
+  test('sign up post', () async {
+	  //User alwaysWork = await UserHelper.loginUser("test0@test.com", "password");
+    //expect( "test0@test.com" , alwaysWork.email);
+
+    User onlyPassOnce = await UserHelper.loginUser("test4@test.com", "password");
+
+    var answer = "test4@test.com";
+    expect(answer, onlyPassOnce.email);
+
+  });
+}
